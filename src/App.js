4ae357281a11ec92a0d15c532c505063e261562a
preeyankae <57045063+preeyankae@users.noreply.github.com>
2022-03-11 12:41:17 +0530

    tailwind init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="bg-sky-600 hover:bg-sky-800 peer">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link peer-focused:bg-sky-400"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
